d882abbdfdc561b178b03e97ef6d0a90a97147f5
Talina Bay <talinabayeleva@gmail.com>
2023-02-04 19:48:58 +1100

    added description to changes of change

--- app/javascript/controllers/stages_of_change_list_controller.js
+++ app/javascript/controllers/stages_of_change_list_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="stages-of-change-list"
 export default class extends Controller {
-  static targets = ["precdescription", "cdescription", "prepdescription", "actiondescription", "maintdescription", "precstagetext", "cstagetext", "prepstagetext", "actionstagetext", "maintstagetext"]
+  static targets = ["precdescription", "cdescription", "prepdescription", "relapsedescription", "maintdescription", "precstagetext", "cstagetext", "prepstagetext", "relapsestagetext", "maintstagetext"]
   connect() {
   }
 
@@ -10,60 +10,60 @@ export default class extends Controller {
     this.precstagetextTarget.classList.add("active-progress")
     this.cstagetextTarget.classList.remove("active-progress")
     this.prepstagetextTarget.classList.remove("active-progress")
-    this.actionstagetextTarget.classList.remove("active-progress")
+    this.relapsestagetextTarget.classList.remove("active-progress")
     this.maintstagetextTarget.classList.remove("active-progress")
     this.precdescriptionTarget.classList.remove("d-none")
     this.cdescriptionTarget.classList.add("d-none")
     this.prepdescriptionTarget.classList.add("d-none")
-    this.actiondescriptionTarget.classList.add("d-none")
+    this.relapsedescriptionTarget.classList.add("d-none")
     this.maintdescriptionTarget.classList.add("d-none")
   }
   cstage() {
     this.precstagetextTarget.classList.remove("active-progress")
     this.cstagetextTarget.classList.add("active-progress")
     this.prepstagetextTarget.classList.remove("active-progress")
-    this.actionstagetextTarget.classList.remove("active-progress")
+    this.relapsestagetextTarget.classList.remove("active-progress")
     this.maintstagetextTarget.classList.remove("active-progress")
     this.precdescriptionTarget.classList.add("d-none")
     this.cdescriptionTarget.classList.remove("d-none")
     this.prepdescriptionTarget.classList.add("d-none")
-    this.actiondescriptionTarget.classList.add("d-none")
+    this.relapsedescriptionTarget.classList.add("d-none")
     this.maintdescriptionTarget.classList.add("d-none")
   }
   prepstage() {
     this.precstagetextTarget.classList.remove("active-progress")
     this.cstagetextTarget.classList.remove("active-progress")
     this.prepstagetextTarget.classList.add("active-progress")
-    this.actionstagetextTarget.classList.remove("active-progress")
+    this.relapsestagetextTarget.classList.remove("active-progress")
     this.maintstagetextTarget.classList.remove("active-progress")
     this.precdescriptionTarget.classList.add("d-none")
     this.cdescriptionTarget.classList.add("d-none")
     this.prepdescriptionTarget.classList.remove("d-none")
-    this.actiondescriptionTarget.classList.add("d-none")
+    this.relapsedescriptionTarget.classList.add("d-none")
     this.maintdescriptionTarget.classList.add("d-none")
   }
-  actionstage() {
+  relapsestage() {
     this.precstagetextTarget.classList.remove("active-progress")
     this.cstagetextTarget.classList.remove("active-progress")
     this.prepstagetextTarget.classList.remove("active-progress")
-    this.actionstagetextTarget.classList.add("active-progress")
+    this.relapsestagetextTarget.classList.add("active-progress")
     this.maintstagetextTarget.classList.remove("active-progress")
     this.precdescriptionTarget.classList.add("d-none")
     this.cdescriptionTarget.classList.add("d-none")
     this.prepdescriptionTarget.classList.add("d-none")
-    this.actiondescriptionTarget.classList.remove("d-none")
+    this.relapsedescriptionTarget.classList.remove("d-none")
     this.maintdescriptionTarget.classList.add("d-none")
   }
   maintstage() {
     this.precstagetextTarget.classList.remove("active-progress")
     this.cstagetextTarget.classList.remove("active-progress")
     this.prepstagetextTarget.classList.remove("active-progress")
-    this.actionstagetextTarget.classList.remove("active-progress")
+    this.relapsestagetextTarget.classList.remove("active-progress")
     this.maintstagetextTarget.classList.add("active-progress")
     this.precdescriptionTarget.classList.add("d-none")
     this.cdescriptionTarget.classList.add("d-none")
     this.prepdescriptionTarget.classList.add("d-none")
-    this.actiondescriptionTarget.classList.add("d-none")
+    this.relapsedescriptionTarget.classList.add("d-none")
     this.maintdescriptionTarget.classList.remove("d-none")
   }
 }
